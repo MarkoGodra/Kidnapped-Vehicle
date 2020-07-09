aa40c4ffede56458ff41d1cc947d453695a6f882
MarkoGodra <marko.godra@gmail.com>
2020-07-09 18:36:41 +0200

    Implement PF initialization

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,8 +30,35 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 150;  // TODO: Set the number of particles
+  double std_x = std[0];
+  double std_y = std[1];
+  double std_theta = std[2];
 
+  std::default_random_engine gen;
+
+    // This line creates a normal (Gaussian) distribution for x
+  std::normal_distribution<double> dist_x(x, std_x);
+  std::normal_distribution<double> dist_y(y, std_y);
+  std::normal_distribution<double> dist_theta(theta, std_theta);
+
+
+  for(auto i = 0; i < num_particles; i++)
+  {
+    // Initialize particle
+    Particle particle;
+    particle.x = dist_x(gen);
+    particle.y = dist_y(gen);
+    particle.theta = dist_theta(gen);
+    particle.weight = 1.0;
+
+    particles.push_back(particle);
+
+    // Initialize coresponding weight
+    weights[i] = 1.0;
+  }
+
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
@@ -43,7 +70,6 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
-
 }
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
